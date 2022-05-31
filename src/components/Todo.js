09162f4a441ff7340ca0f10edce90b2272f5ca2e
buithaihung <buithaihung6934@gmail.com>
2022-05-31 20:10:31 +0700

    Change Todo Status

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 /* 
   【Todoのデータ構成】
@@ -8,39 +8,40 @@ import React, { useState } from 'react';
 */
 
 /* コンポーネント */
-import TodoItem from './TodoItem';
-import Input from './Input';
-import Filter from './Filter';
+import TodoItem from "./TodoItem";
+import Input from "./Input";
+import Filter from "./Filter";
 
 /* カスタムフック */
-import useStorage from '../hooks/storage';
+import useStorage from "../hooks/storage";
 
 /* ライブラリ */
-import {getKey} from "../lib/util";
+import { getKey } from "../lib/util";
 
 function Todo() {
   const [items, putItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
+    /* テストコード 開始 */
+    { key: getKey(), text: "日本語の宿題", done: false },
+    { key: getKey(), text: "reactを勉強する", done: false },
+    { key: getKey(), text: "明日の準備をする", done: false },
     /* テストコード 終了 */
   ]);
-
+  const handleCheck = (checked) => {
+    const newItems = items.map((item) => {
+      if (item.key === checked.key) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    putItems(newItems);
+  };
   return (
     <div className="panel">
-      <div className="panel-heading">
-        ITSS ToDoアプリ
-      </div>
-      {items.map(item => (
-        <label className="panel-block">
-            <input type="checkbox" />
-            {item.text}
-        </label>
+      <div className="panel-heading">ITSS ToDoアプリ</div>
+      {items.map((item) => (
+        <TodoItem key={item.key} item={item} onCheck={handleCheck} />
       ))}
-      <div className="panel-block">
-        {items.length} items
-      </div>
+      <div className="panel-block">{items.length} items</div>
     </div>
   );
 }
